6bf9f00c9fdf6a0cfc19af9fa56618a5be90949a
somenath <somnath2prl@gmail.com>
2014-06-17 20:45:42 +0530

    New module added

---
+++ modules/voxeu_jobs/templates/filter_form.tpl.php
@@ -0,0 +1,32 @@
+<table class='filter-form'>
+  <tbody>
+    <tr>
+      <td>
+        <?php print $form['categories']['#title'];?>
+      </td>
+      <td>
+        <?php print drupal_render($form['categories']);?>
+      </td>
+      <td row	span='2' width='80' align='center'>
+        <?php print drupal_render($form['submit']);?>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <?php print $form['types']['#title'];?>
+      </td>
+      <td>
+        <?php print drupal_render($form['types']);?>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<?php if(isset($form['table'])): ?>
+  <?php print drupal_render($form['table']);?>
+<?php endif; ?>
+<?php
+  print drupal_render($form['form_build_id']);
+  print drupal_render($form['form_token']);
+  print drupal_render($form['form_id']);
+?>
